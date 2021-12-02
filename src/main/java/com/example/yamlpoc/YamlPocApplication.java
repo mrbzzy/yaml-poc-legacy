fb8cf0a268d76f9a03964993f3a351ddf3b186aa
mrbzzy <38424081+mrbzzy@users.noreply.github.com>
2021-12-02 14:23:20 +0530

    Add files via upload

---
+++ src/main/java/com/example/yamlpoc/YamlPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.yamlpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamlPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YamlPocApplication.class, args);
+	}
+
+}
