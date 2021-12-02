8e59c42d92a87bf8346cc6ec814dbefaebfb7275
mrbzzy <38424081+mrbzzy@users.noreply.github.com>
2021-12-02 14:29:24 +0530

    Update YamlPocApplication.java

--- src/main/java/com/example/yamlpoc/YamlPocApplication.java
+++ src/main/java/com/example/yamlpoc/YamlPocApplication.java
@@ -2,10 +2,18 @@ package com.example.yamlpoc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class YamlPocApplication {
-
+	
+	@GetMapping("/yamlpoc") 
+	public String yamlpoc(){
+		return "yaml-poc successfull via Spring boot demo application";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(YamlPocApplication.class, args);
 	}
